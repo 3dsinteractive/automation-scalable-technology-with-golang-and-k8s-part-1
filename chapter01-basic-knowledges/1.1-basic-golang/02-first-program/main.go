f50b697e265c1d349555cdbdcdb4b8b1255a27ad
pong3ds <chaiyapong@3dsinteractive.com>
2021-01-23 19:16:40 +0700

    Add comment in first program

--- chapter01-basic-knowledges/1.1-basic-golang/02-first-program/main.go
+++ chapter01-basic-knowledges/1.1-basic-golang/02-first-program/main.go
@@ -1,7 +1,10 @@
+// 1. Package name must be main for main package
 package main
 
+// 2. Import section is the list of package dependency
 import "fmt"
 
+// 3. main() is the function where application start
 func main() {
 	fmt.Println("My First Program")
 }
