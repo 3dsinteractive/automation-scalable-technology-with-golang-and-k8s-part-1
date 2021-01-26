65cab1a7d26a776254274f3d1485222f0fd23401
pong3ds <chaiyapong@3dsinteractive.com>
2021-01-26 21:19:49 +0700

    Add README and comments

--- chapter03-deploy-scale-services/3.1-deploy-services/main.go
+++ chapter03-deploy-scale-services/3.1-deploy-services/main.go
@@ -2,25 +2,13 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
 	"net/http"
-	"time"
 )
 
 func main() {
 	ms := NewMicroservice()
 
 	startHTTP(ms)
-	startConsumer(ms)
-	startBatchConsumer(ms)
-	exitSC := startScheduler(ms)
-	startAsyncTask(ms)
-	startParallelTask(ms)
-
-	defer func() {
-		exitSC <- true
-	}()
 
 	ms.Start()
 }
@@ -66,103 +54,4 @@ func startHTTP(ms *Microservice) {
 		ctx.Response(http.StatusOK, status)
 		return nil
 	})
-}
-
-func startConsumer(ms *Microservice) {
-	servers := "localhost:9094"
-	topic := "when-citizen-has-registered-" + randString()
-	groupID := "validation-consumer"
-	timeout := time.Duration(-1)
-
-	ms.Consume(servers, topic, groupID, timeout, func(ctx IContext) error {
-		msg := ctx.ReadInput()
-		ctx.Log(msg)
-		return nil
-	})
-
-	prod := ms.getProducer(servers)
-	go func() {
-		for i := 0; i < 10; i++ {
-			prod.SendMessage(topic, "", map[string]interface{}{"message_id": i})
-			time.Sleep(time.Second)
-		}
-
-		// Exit program
-		ms.Stop()
-	}()
-}
-
-func startBatchConsumer(ms *Microservice) {
-	mqServers := "localhost:9094"
-	topic := "when-citizen-has-registered-batch-" + randString()
-	groupID := "validation-consumer"
-	timeout := time.Duration(-1)
-	batchSize := 3
-	batchTimeout := time.Second * 5
-
-	ms.ConsumeBatch(mqServers, topic, groupID, timeout, batchSize, batchTimeout,
-		func(ctx IContext) error {
-			msgs := ctx.ReadInputs()
-			ctx.Log("Begin Batch")
-			for _, msg := range msgs {
-				ctx.Log(msg)
-			}
-			ctx.Log("End Batch")
-			return nil
-		})
-
-	prod := ms.getProducer(mqServers)
-	go func() {
-		for i := 0; i < 10; i++ {
-			prod.SendMessage(topic, "", map[string]interface{}{"message_id": i})
-			time.Sleep(time.Second)
-		}
-
-		// Wait for last batch then exit program
-		time.Sleep(5 * time.Second)
-		ms.Stop()
-	}()
-}
-
-func startScheduler(ms *Microservice) chan bool {
-	timer := 1 * time.Second
-	exitScheduler := ms.Schedule(timer, func(ctx IContext) error {
-		now := time.Now()
-		ctx.Log(fmt.Sprintf("Tick at %s", now.Format("15:04:05")))
-		return nil
-	})
-	return exitScheduler
-}
-
-func startAsyncTask(ms *Microservice) {
-	cacheServer := "localhost:6379"
-	mqServers := "localhost:9094"
-	ms.AsyncPOST("/citizen/register", cacheServer, mqServers, func(ctx IContext) error {
-		ctx.Log(ctx.ReadInput())
-		res := map[string]interface{}{
-			"id": "123",
-		}
-		ctx.Response(http.StatusOK, res)
-		return nil
-	})
-}
-
-func startParallelTask(ms *Microservice) {
-	cacheServer := "localhost:6379"
-	mqServers := "localhost:9094"
-	ms.PTask("/citizen/batch", cacheServer, mqServers)
-
-	// Start 3 workers
-	for i := 0; i < 3; i++ {
-		ms.PTaskWorker("/citizen/batch", cacheServer, mqServers, func(ctx IContext) error {
-			ctx.Log(ctx.ReadInput())
-			res := map[string]interface{}{
-				"id": "123",
-			}
-			n := rand.Intn(5)
-			time.Sleep(time.Duration(n) * time.Second)
-			ctx.Response(http.StatusOK, res)
-			return nil
-		})
-	}
 }
